578002629652b9777ca0ae757e92c010faeca1a3
Basil Goodluck <basilgoodluck22@gmail.com>
2023-12-20 02:56:31 +0100

    Updated

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("body .headerContainer #headerContainer")
+
+var sticky = header.offsetTop;
+
+window.onscroll = function() {
+    if (window.scrollY > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  };
