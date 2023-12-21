c96783073a2923041e3ef14bc6bc692026484613
Basil Goodluck <basilgoodluck22@gmail.com>
2023-12-21 01:55:32 +0100

    Updated

--- assets/script.js
+++ assets/script.js
@@ -12,5 +12,22 @@
 
 // }
 
-// clike()
+// // clike()
+// header = document.querySelector("#headerContainer")
 
+// if (scrollPosition >= header) {
+//     header.style.position = 'fixed';
+//     header.style.top = '0'; // Ensure header stays at the top
+//   } else {
+//     header.style.position = 'static'; // Reset to original position when scrolled back up
+//   }
+var header = document.getElementById("headerContainer");
+var sticky = header.offsetTop;
+
+window.onscroll = function() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
