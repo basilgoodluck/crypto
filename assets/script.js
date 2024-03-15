821fdc16b098fd6f81ff7965fbc002182f6b8bd8
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-15 22:26:30 +0100

    Updated

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 const navBar = document.querySelector('.mobile-nav-btn')
+let mobileCon = document.querySelector('.mobile-nav')
+let menu = document.querySelector('.nav-menu')
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileCon = document.querySelector('.mobile-nav');
+    const menu = document.getElementById('nav-menu');
+
+    const clonedNavMenu = menu.cloneNode(true);
+    clonedNavMenu.id = 'clonee';
+
+    document.mobileCon.appendChild(clonedNavMenu);
+
+    console.log(mobileCon);
+});
 function toggleNav () {
     navBar.classList.toggle('active')
 }
