0d41ac39ceb125d029306c082d5884eee7fb866d
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-15 09:14:11 +0100

    Updated

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,5 @@
-// // 
-// const about = document.getElementById("about")
-// const header = document.getElementById("mainHeaderContainer")
+const navBar = document.querySelector('.mobile-nav-btn')
 
-// offSetHead = header.offsetHeight;
-
-// const clike = function(){
-//     window.scrollTo({
-//         top: about.offsetTop + offSetHead,
-//         behavior: 'smooth' // Optional for smooth scrolling
-//     });
-
-// }
-
-// // clike()
-// header = document.querySelector("#headerContainer")
-
-// if (scrollPosition >= header) {
-//     header.style.position = 'fixed';
-//     header.style.top = '0'; // Ensure header stays at the top
-//   } else {
-//     header.style.position = 'static'; // Reset to original position when scrolled back up
-//   }
-// var header = document.getElementById("headerContainer");
-// var sticky = header.offsetTop;
-
-// window.onscroll = function() {
-//   if (window.scrollY > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// };
-
-
-
-
-
+function toggleNav () {
+    navBar.classList.toggle('active')
+}
