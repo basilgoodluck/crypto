61db6671e9912f17a3f71697487ba413fb40395c
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-16 11:23:46 +0100

    Updated

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,6 @@
 const navBar = document.querySelector('.mobile-nav-btn')
 let mobileCon = document.querySelector('.mobile-nav')
-let menu = document.querySelector('.nav-menu')
 
-
-const gra = menu.cloneNode(true)
-
-gra.id = 'gra'
-
-mobileCon.appendChild(gra)
-console.log(gra)
 
 
 function toggleNav () {
@@ -18,14 +10,6 @@ function toggleNav () {
 }
 
 
-const filmScreenp = document.getElementById('filmScreenp').innerHTML
-const filmScreenh = document.getElementById('filmScreenh').innerHTML
 
-const reviewArr = ['They are the best', 'We love them', 'Great are they']
-function slideShow () {
-    
-}
-filmScreenh == reviewArr[2]
-console.log(filmScreenh)
 
 
