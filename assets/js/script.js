7311e9b6d3931353d15b2440a115ebfd20330aae
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-16 16:18:51 +0100

    Updated

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const navBar = document.querySelector('.mobile-nav-btn')
+let mobileCon = document.querySelector('.mobile-nav') 
+const filmP = document.querySelector('.filmP')
+const filmH = document.querySelector('.filmH')
+
+
+
+function toggleNav () {
+    navBar.classList.toggle('active')
+    mobileCon.classList.toggle('active')
+
+}
+
+
+const arrT = ['we are the one', 'hajkdhfjdanfjadl', 'adfvakjndknadkf']
+
+
+let i = 0;
+
+while (i < arrT.length){
+    let a = i;
+    setTimeout(()=>{
+        filmH = arrT[a];
+    }, 1000)
+    i++
+}
+
+
+
+
+
+
