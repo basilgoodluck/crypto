bad28074e0373528add8afd2e1f6ff05fcc56d84
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-18 16:08:26 +0100

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const navBar = document.querySelector('.mobile-nav-btn')
 let mobileCon = document.querySelector('.mobile-nav') 
-const filmP = document.querySelector('.filmP')
-const filmH = document.querySelector('.filmH')
+let filmP = document.querySelector('.filmP')
+let filmH = document.querySelector('.filmH')
+let BTNS = document.querySelectorAll('.nav-slide-btn')
+let contai = document.querySelector('#filmScreenp')
+let contjdj = document.querySelector('#filmScreenh')
+let ani = document.querySelector('.review-content .review-text')
 
 
 const toggleNav = function () {
@@ -10,22 +14,23 @@ const toggleNav = function () {
 
 }
 
-
-const arrT = ['we are the one', 'hajkdhfjdanfjadl', 'adfvakjndknadkf']
-
-
-let i = 0;
-
-while (i < arrT.length){
-    let a = i;
-    setTimeout(()=>{
-        filmH = arrT[a];
-    }, 1000)
-    i++
-}
-
-
-
-
-
-
+window.addEventListener('DOMContentLoaded', function () {
+    let contentBox = document.querySelectorAll('.review-text')
+    let idx = 0
+    function nextContent () {
+        
+        
+        contentBox[idx].classList.remove('active')
+        contentBox[idx + 1].classList.add('active') 
+        idx++
+    }
+    function prevContent () {
+        contentBox[idx].classList.remove('active')
+        // contentBox[idx].classList.add('active')
+        // idx--
+         
+    }
+    // setInterval(updateContent, 1000)
+    document.getElementById('prevBTN').addEventListener('click', prevContent)
+    document.getElementById('nextBTN').addEventListener('click', nextContent)
+})
