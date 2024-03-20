a2967bcc9a1154a9e2975b52f52bf6b98a9c7c3c
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-21 00:19:31 +0100

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
 const navBar = document.querySelector('.mobile-nav-btn')
 let mobileCon = document.querySelector('.mobile-nav') 
-let filmP = document.querySelector('.filmP')
-let filmH = document.querySelector('.filmH')
-let BTNS = document.querySelectorAll('.nav-slide-btn')
-let slideContainer = document.querySelector('.review-content')
-let slides = document.querySelectorAll('.review-text')
+const slideBox = document.querySelector('.slidesBox')
+const slides = document.querySelectorAll('.slide')
 
+const slideWidth = slideBox.clientWidth
+let index = 1;
+console.log(slideWidth)
 
-const toggleNav = function () {
-    navBar.classList.toggle('active')
-    mobileCon.classList.toggle('active')
+slideBox.style.transform = `translateX(${-slideWidth * index}px)`
 
-}
 
 const firstClone = slides[0].cloneNode(true)
 const lastClone = slides[slides.length - 1].cloneNode(true)
@@ -19,10 +16,16 @@ const lastClone = slides[slides.length - 1].cloneNode(true)
 firstClone.id = 'first-clone'
 lastClone.id = 'last-clone'
 
-slideContainer.append(firstClone)
-slideContainer.prepend(lastClone)
+slideBox.append(firstClone)
+slideBox.prepend(lastClone)
 
 // slideContainer.style.transform = `translateX(-50%)`
+console.log(slideBox)
 console.log(slides)
 
 
+const toggleNav = function () {
+    navBar.classList.toggle('active')
+    mobileCon.classList.toggle('active')
+
+}
