18eaa3de460871ec9bb26fd6f7ba0ef250f68e28
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-21 01:34:56 +0100

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 const navBar = document.querySelector('.mobile-nav-btn')
 let mobileCon = document.querySelector('.mobile-nav') 
 const slideBox = document.querySelector('.slidesBox')
-const slides = document.querySelectorAll('.slide')
+let slides = document.querySelectorAll('.slide')
 
-const slideWidth = slideBox.clientWidth
+let slideWidth = slideBox.clientWidth
+let newWidth
+window.addEventListener('resize', ()=>{
+     newWidth = slideBox.clientWidth
+     slideWidth = newWidth
+})
 let index = 1;
 console.log(slideWidth)
 
-slideBox.style.transform = `translateX(${-slideWidth * index}px)`
+// slideBox.style.transform = `translateX(${-slideWidth * index}px)`
 
 
 const firstClone = slides[0].cloneNode(true)
@@ -23,7 +28,29 @@ slideBox.prepend(lastClone)
 console.log(slideBox)
 console.log(slides)
 
+function updateSlide () {
+    index++
+    slides = document.querySelectorAll('.slide')
+    if (index > slides.length){
+        index = 1
+    } 
+    
+    slideBox.style.transform = `translateX(${-slideWidth * index}px)`
+    slideBox.style.transition = '.7s'
+       
+}
 
+slideBox.addEventListener('transitionend', ()=>{
+    slides = document.querySelectorAll('.slide')
+
+    if(slides[index].id === firstClone.id){
+        slideBox.style.transition = 'none'
+        index = 1
+        slideBox.style.transform = `translateX(${-slideWidth * index}px)`
+    }
+})
+// window.addEventListener()
+setInterval(updateSlide, 3000)
 const toggleNav = function () {
     navBar.classList.toggle('active')
     mobileCon.classList.toggle('active')
